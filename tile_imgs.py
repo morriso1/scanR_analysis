518ea80b08f51c7fc07880f6de7ab77e0b3f8ea0
Otto Morris <ottomorris@gmail.com>
2021-04-13 10:00:06 +0100

    added spiral_right and spiral_left

--- tile_imgs.py
+++ tile_imgs.py
@@ -15,3 +15,55 @@ def create_matrix_zigzag_row(rowCount, colCount, dataList):
                 rowList.append(dataList[index])
             mat.append(rowList)
     return mat
+
+
+def spiral_left(width, height):
+    NORTH, S, W, E = (0, -1), (0, 1), (-1, 0), (1, 0)  # directions
+    turn_left = {NORTH: W, W: S, S: E, E: NORTH}
+    if width < 1 or height < 1:
+        raise ValueError
+    x, y = width // 2, height // 2  # start near the center
+    dx, dy = NORTH  # initial direction
+    matrix = [[None] * width for _ in range(height)]
+    count = 0
+    while True:
+        matrix[y][x] = count  # visit
+        count += 1
+        # try to turn left
+        new_dx, new_dy = turn_left[dx, dy]
+        new_x, new_y = x + new_dx, y + new_dy
+        if (
+            0 <= new_x < width and 0 <= new_y < height and matrix[new_y][new_x] is None
+        ):  # can turn right
+            x, y = new_x, new_y
+            dx, dy = new_dx, new_dy
+        else:  # try to move straight
+            x, y = x + dx, y + dy
+            if not (0 <= x < width and 0 <= y < height):
+                return matrix  # nowhere to go
+
+
+def spiral_right(width, height):
+    NORTH, S, W, E = (0, -1), (0, 1), (-1, 0), (1, 0)  # directions
+    turn_right = {NORTH: E, E: S, S: W, W: NORTH}  # old -> new direction
+    if width < 1 or height < 1:
+        raise ValueError
+    x, y = width // 2, height // 2  # start near the center
+    dx, dy = NORTH  # initial direction
+    matrix = [[None] * width for _ in range(height)]
+    count = 0
+    while True:
+        matrix[y][x] = count  # visit
+        count += 1
+        # try to turn right
+        new_dx, new_dy = turn_right[dx, dy]
+        new_x, new_y = x + new_dx, y + new_dy
+        if (
+            0 <= new_x < width and 0 <= new_y < height and matrix[new_y][new_x] is None
+        ):  # can turn right
+            x, y = new_x, new_y
+            dx, dy = new_dx, new_dy
+        else:  # try to move straight
+            x, y = x + dx, y + dy
+            if not (0 <= x < width and 0 <= y < height):
+                return matrix  # nowhere to go
