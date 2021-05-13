dc47245febcb4dedb52fe69afe37b1cd3e391875
Otto Morris <ottomorris@gmail.com>
2021-05-13 18:42:41 +0100

    updated to include new spiral functions

--- tile_imgs.py
+++ tile_imgs.py
@@ -17,53 +17,27 @@ def create_matrix_zigzag_row(rowCount, colCount, dataList):
     return mat
 
 
-def spiral_left(width, height):
-    NORTH, S, W, E = (0, -1), (0, 1), (-1, 0), (1, 0)  # directions
-    turn_left = {NORTH: W, W: S, S: E, E: NORTH}
-    if width < 1 or height < 1:
-        raise ValueError
-    x, y = width // 2, height // 2  # start near the center
-    dx, dy = NORTH  # initial direction
-    matrix = [[None] * width for _ in range(height)]
-    count = 0
-    while True:
-        matrix[y][x] = count  # visit
-        count += 1
-        # try to turn left
-        new_dx, new_dy = turn_left[dx, dy]
-        new_x, new_y = x + new_dx, y + new_dy
-        if (
-            0 <= new_x < width and 0 <= new_y < height and matrix[new_y][new_x] is None
-        ):  # can turn right
-            x, y = new_x, new_y
-            dx, dy = new_dx, new_dy
-        else:  # try to move straight
-            x, y = x + dx, y + dy
-            if not (0 <= x < width and 0 <= y < height):
-                return matrix  # nowhere to go
+def spiral_cw(A):
+    import numpy as np
+
+    A = np.array(A)
+    out = []
+    while A.size:
+        out.append(A[:, 0])  # take first column
+        A = A[:, 1:].T[:, ::-1]  # cut off first column and rotate counterclockwise
+    return np.concatenate(out)
 
 
-def spiral_right(width, height):
-    NORTH, S, W, E = (0, -1), (0, 1), (-1, 0), (1, 0)  # directions
-    turn_right = {NORTH: E, E: S, S: W, W: NORTH}  # old -> new direction
-    if width < 1 or height < 1:
-        raise ValueError
-    x, y = width // 2, height // 2  # start near the center
-    dx, dy = NORTH  # initial direction
-    matrix = [[None] * width for _ in range(height)]
-    count = 0
-    while True:
-        matrix[y][x] = count  # visit
-        count += 1
-        # try to turn right
-        new_dx, new_dy = turn_right[dx, dy]
-        new_x, new_y = x + new_dx, y + new_dy
-        if (
-            0 <= new_x < width and 0 <= new_y < height and matrix[new_y][new_x] is None
-        ):  # can turn right
-            x, y = new_x, new_y
-            dx, dy = new_dx, new_dy
-        else:  # try to move straight
-            x, y = x + dx, y + dy
-            if not (0 <= x < width and 0 <= y < height):
-                return matrix  # nowhere to go
+def base_spiral(nrow, ncol):
+    import numpy as np
+
+    return spiral_cw(np.arange(nrow * ncol).reshape(nrow, ncol))[::-1]
+
+
+def to_spiral(nrows, ncols):
+    import numpy as np
+
+    A = np.arange(nrows * ncols).reshape(nrows, ncols)
+    B = np.empty_like(A)
+    B.flat[base_spiral(*A.shape)] = A.flat
+    return np.flip(B, axis=1)
